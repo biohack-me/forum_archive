59cd6eca830d77adf829a51117625ef22700183b
tekniklr <tekniklr@tekniklr.com>
2024-06-06 02:27:45 -0700

    tidy up user javascript

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,15 @@
-function toggle_discussions() {
-  $("#user_discussions").fadeToggle();
-  $("#toggle_discussions").text($("#toggle_discussions").text() == 'show discussions' ? 'hide discussions' : 'show discussions');
-}
+$(function() {
 
-function toggle_comments() {
-  $("#user_comments").fadeToggle();
-  $("#toggle_comments").text($("#toggle_comments").text() == 'show comments' ? 'hide comments' : 'show comments');
-}
+  $("#toggle_discussions").click(function() {
+    $("#user_discussions").fadeToggle();
+    $("#toggle_discussions").text($("#toggle_discussions").text() == 'show discussions' ? 'hide discussions' : 'show discussions');
+    return false;
+  });
+
+  $("#toggle_comments").click(function() {
+    $("#user_comments").fadeToggle();
+    $("#toggle_comments").text($("#toggle_comments").text() == 'show comments' ? 'hide comments' : 'show comments');
+    return false;
+  });
+
+});
