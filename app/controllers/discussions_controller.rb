bbdb986e1a302eeecfc3875380666e0a733c53a2
tekniklr <tekniklr@tekniklr.com>
2024-07-20 09:41:37 -0700

    don't crash when people attempt SQL injections (which didn't work, but `.to_i` made them '0', which wasn't valid in this context)

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,7 +2,8 @@ class DiscussionsController < ApplicationController
   caches_action :show, expires_in: CACHE_TIME, race_condition_ttl: CACHE_TTL, cache_path: Proc.new { |c| "#{c.params[:id]}_#{c.params[:page].to_i}" }
 
   def show
-    params[:page] ||= 1
+    params[:page] ||= '1'
+    (params[:page].to_i > 0) or params[:page] = '1'
     @discussion = Discussion.find(params[:id])
     @page_title = @discussion.name
   end
