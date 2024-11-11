7db5c6c0c8eb4eab1d6486c6f2be6e3890bb173a
tekniklr <tekniklr@tekniklr.com>
2024-11-11 14:36:18 -0800

    rails 8.0.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem 'rails', '~>7.2.0'
+gem 'rails', '~>8.0.0'
 gem 'mysql2'
 gem 'puma'
 
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module ForumArchive
   class Application < Rails::Application
     config.load_defaults 7.1
     config.autoload_lib(ignore: %w(assets tasks))
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
