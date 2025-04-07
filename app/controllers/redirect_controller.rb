66a073604efdc41dadba82b38b3dd8e4e07380a2
tekniklr <tekniklr@tekniklr.com>
2025-04-07 14:22:44 -0700

    cache the logic for index.php redirection

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,55 +1,67 @@
 class RedirectController < ApplicationController
 
   def redirect
-    params[:p].blank? and raise ActionController::RoutingError, 'Not Found'
 
-    logger.debug "************ figuring out where to redirect #{params[:p]} to..."
+    # cache each set of params so we only do the logic once
+    redirect_action = Rails.cache.fetch("redirect_for_#{params[:p]}") do
+      redirect_path = false
+      not_found = false
 
-    # looking for category?
-    if params[:p] =~ /\A\/categories\/(\w+)/
-      logger.debug "************ a category!"
-      category_name = $1
-      category = Category.find_by_name(category_name)
-      if !category.blank?
-        logger.debug "************ redirecting to #{category.name}..."
-        return redirect_to category_path(category), status: :moved_permanently
-      else
-        logger.debug "************ but no category called '#{category_name}' was found..."
-        raise ActionController::RoutingError, 'Not Found'
+      params[:p].blank? and not_found = true
+
+      logger.debug "************ figuring out where to redirect #{params[:p]} to..."
+
+      # looking for category?
+      if !not_found && (params[:p] =~ /\A\/categories\/(\w+)/)
+        logger.debug "************ a category!"
+        category_name = $1
+        category = Category.find_by_name(category_name)
+        if !category.blank?
+          logger.debug "************ redirecting to #{category.name}..."
+          redirect_path = category_path(category)
+        else
+          logger.debug "************ but no category called '#{category_name}' was found..."
+          not_found = true
+        end
       end
+
+      # looking for discussion?
+      if !not_found && (params[:p] =~ /\A\/discussion\/([0-9]+)\//)
+        logger.debug "************ a discussion!"
+        discussion_id = $1
+        discussion = Discussion.where('DiscussionID = ?', discussion_id).first
+        if !discussion.blank?
+          logger.debug "************ redirecting to #{discussion.name}..."
+          redirect_path = discussion_path(discussion)
+        else
+          logger.debug "************ but no discussion with id '#{discussion_id}' was found..."
+          not_found = true
+        end
+      end
+
+      # looking for user profile?
+      if !not_found && (params[:p] =~ /\A\/profile\/(\w+)/)
+        logger.debug "************ a user profile!"
+        user_name = $1
+        user = User.find_by_name(user_name)
+        if !user.blank?
+          logger.debug "************ redirecting to #{user.name}..."
+          redirect_path = user_path(user)
+        else
+          logger.debug "************ but no user called '#{user_name}' was found..."
+          not_found = true
+        end
+      end
+
+      redirect_path ? redirect_path : false
     end
 
-    # looking for discussion?
-    if params[:p] =~ /\A\/discussion\/([0-9]+)\//
-      logger.debug "************ a discussion!"
-      discussion_id = $1
-      discussion = Discussion.where('DiscussionID = ?', discussion_id).first
-      if !discussion.blank?
-        logger.debug "************ redirecting to #{discussion.name}..."
-        return redirect_to discussion_path(discussion), status: :moved_permanently
-      else
-        logger.debug "************ but no discussion with id '#{discussion_id}' was found..."
-        raise ActionController::RoutingError, 'Not Found'
-      end
+    if redirect_action
+      return redirect_to redirect_action, status: :moved_permanently
+    else
+      raise ActionController::RoutingError, 'Not Found'
     end
 
-    # looking for user profile?
-    if params[:p] =~ /\A\/profile\/(\w+)/
-      logger.debug "************ a user profile!"
-      user_name = $1
-      user = User.find_by_name(user_name)
-      if !user.blank?
-        logger.debug "************ redirecting to #{user.name}..."
-        return redirect_to user_path(user), status: :moved_permanently
-      else
-        logger.debug "************ but no user called '#{user_name}' was found..."
-        raise ActionController::RoutingError, 'Not Found'
-      end
-    end
-
-
-    logger.warn "************ didn't understand how to redirect #{params[:p]} :("
-    raise ActionController::RoutingError, 'Not Found'
   end
 
 end
