b196337196967e67e6d96c4e4f4e5cc69f6f343c
tekniklr <tekniklr@tekniklr.com>
2025-04-07 03:23:24 -0700

    use rack::attack to actually ban IPs that are hammering the site with their scraping and poking

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,7 +4,7 @@ class Rack::Attack
 
   # prevent people/bots from hitting all the moved vanilla URLs overly quickly
   throttle('redirect_throttling', limit: 5, period: 1.minute) do |req|
-    if (req.path =~ /index.php/) 
+    if req.path.include?('index.php')
       req.ip
     end
   end
@@ -29,5 +29,23 @@ ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, 
   req = payload[:request]
   if req.env['rack.attack.matched'] == "redirect_throttling"
     Rails.logger.warn "************ Vanilla URL redirect rate limit exceeded!\n************\tpath: #{req.path}\n************\tquery: #{req.params}"
+  end
+end
+
+# Block suspicious requests
+# After 2 blocked requests in 10 minutes, block all requests from that IP for
+# 1 hour.
+Rack::Attack.blocklist('fail2ban shenanigans') do |req|
+  Rack::Attack::Fail2Ban.filter("tomfoolery-#{req.ip}", maxretry: 2, findtime: 10.minutes, bantime: 3.hours) do
+    req.path.include?('bad_route.html') ||
+    req.path.include?('wp-admin') ||
+    req.path.include?('wp-login')
+  end
+end
+
+# Actually block people who keep going to index.php URLs past the throttling
+Rack::Attack.blocklist('fail2ban index.php') do |req|
+  Rack::Attack::Allow2Ban.filter("indexers-#{req.ip}", maxretry: 10, findtime: 3.minutes, bantime: 3.hours) do
+    req.path.include?('index.php')
   end
 end
