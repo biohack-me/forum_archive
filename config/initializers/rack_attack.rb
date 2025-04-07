22a59b15606918bb03ac000fc6c228d227cd53fc
tekniklr <tekniklr@tekniklr.com>
2025-04-07 15:00:39 -0700

    try to fix rack::attack for when server is behind proxy

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,11 +1,18 @@
 class Rack::Attack
 
+  # see: https://github.com/rack/rack-attack/issues/145
+  class Request < ::Rack::Request
+    def remote_ip
+      @remote_ip ||= (env['action_dispatch.remote_ip'] || env['HTTP_X_FORWARDED_FOR'] || ip).to_s
+    end
+  end
+
   ### Throttle config (prevents user for getting further responses) ###
 
   # prevent people/bots from hitting all the moved vanilla URLs overly quickly
   throttle('redirect_throttling', limit: 5, period: 1.minute) do |req|
     if req.path.include?('index.php')
-      req.ip
+      req.remote_ip
     end
   end
 
@@ -28,6 +35,23 @@ end
 ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, finish, request_id, payload|
   req = payload[:request]
   if req.env['rack.attack.matched'] == "redirect_throttling"
-    Rails.logger.warn "************ Vanilla URL redirect rate limit exceeded!\n************\tpath: #{req.path}\n************\tquery: #{req.params}"
+    Rails.logger.warn "************ Vanilla URL redirect rate limit exceeded!\n************\tIP: #{req.remote_ip}\n************\tpath: #{req.path}\n************\tquery: #{req.params}"
+  end
+end
+
+# Block suspicious requests
+# After 2 blocked requests in 10 minutes, block all requests from that IP for
+# 1 hour.
+Rack::Attack.blocklist('fail2ban shenanigans') do |req|
+  Rack::Attack::Fail2Ban.filter("tomfoolery-#{req.remote_ip}", maxretry: 2, findtime: 10.minutes, bantime: 3.hours) do
+    req.path.include?('wp-admin') ||
+    req.path.include?('wp-login')
+  end
+end
+
+# Actually block people who keep going to index.php URLs past the throttling
+Rack::Attack.blocklist('fail2ban index.php') do |req|
+  Rack::Attack::Allow2Ban.filter("indexers-#{req.remote_ip}", maxretry: 5, findtime: 3.minutes, bantime: 3.hours) do
+    req.path.include?('bad_route.html')
   end
 end
