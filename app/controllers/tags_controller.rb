07046573891403e9a61ebec88fe899247aa749ee
tekniklr <tekniklr@tekniklr.com>
2026-07-02 11:22:02 -0700

    Return more complete results with tag search
    closes #26

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,10 +3,12 @@ class TagsController < ApplicationController
   def show
     @results = []
     if params[:tag] && !params[:tag].empty?
-      tag_object = Tag.by_name(params[:tag]).first
-      search_tag = tag_object.blank? ? params[:tag] : tag_object.id
-      @results = Rails.cache.fetch("tagged_#{search_tag}", expires_in: 12.hours) do
-                  Discussion.tagged(search_tag).sorted.to_a
+      tag_string = params[:tag]
+      tag_object = Tag.by_name(tag_string).first
+      @results = Rails.cache.fetch("tagged_#{tag_string}", expires_in: 12.hours) do
+                  tagged_with_string = Discussion.tagged(tag_string).sorted.to_a
+                  tagged_with_id     = tag_object ? Discussion.tagged(tag_object.id).sorted.to_a : []
+                  tagged_with_string + tagged_with_id
                 end
     end
   end
